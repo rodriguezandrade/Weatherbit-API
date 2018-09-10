99415eeaf419866e92f4f74ed786b355e7c1f519
DESKTOP-S1AQBDT\Pesebre <1140117@unav.edu.mx>
2018-09-09 23:22:19 -0700

    add tittle from array and assign value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,89 +12,103 @@ import { HttpClient } from '@angular/common/http';
   providers:[WeatherService]
 })
         export class AppComponent {
+          ciudadcodigo: string="4013704";
+  unidad : string;
+  jsonList : any;
+
+
         chart=[ ];
           title = 'dashboardWeather';
              cities=[];
+             units=[];
+             unitsstatus:string="";
+              codecity:any[];
+              units2:any[];
            maxima:any[ ];
             id:string="";
             response:any[ ];
           constructor(private http:HttpClient,private _weather:WeatherService ){
 
         this.cities=this._weather.getCities()
+        this.units=this._weather.getUnits()
+        this._weather.default();
 
         this._weather.daylyForecast()
         .subscribe(res=>{
         let max_temp=res['data'] 
-        this.maxima=max_temp   
+        
         console.log(this.maxima)
       })
+ 
        }
-       Filter(event:any){
-        this.http.get("https://api.weatherbit.io/v2.0/forecast/daily?days=15&units=M&key=3dd1694285a347d4ac8c5c20608f68de&city_id="+event)
+
+
+          
+          Filter(ciudadcodigo : string, unidad : string): void {
+      //  Filter(event:any){
+        this.ciudadcodigo=ciudadcodigo;
+        this.unidad = unidad;
+        console.log(this.ciudadcodigo) 
+         
+        this.unitsstatus='https://api.weatherbit.io/v2.0/forecast/daily?days=15&units=M&key=3dd1694285a347d4ac8c5c20608f68de&city_id='+this.ciudadcodigo;
+        this.http.get(this.unitsstatus)
         .subscribe((res)=> {
           let response=res['data'] ;
+          this.maxima=response  
           this.response=response
-         console.log(this.response);
-        })
+         //console.log(this.response);
+         let max_temp=res['data'].map(res=>res.max_temp)
+         let min_temp=res['data'].map(res=>res.min_temp)
+         let alldates=res['data'].map(res=>res.valid_date)
+         // console.log(alldates)
+         
+     this.chart=new Chart('canvas',{
+     type:'line',
+     data:{
+       labels:alldates,
+         datasets:[{
+           data:max_temp,
+           borderColor:'#0288D1',
+           fill:false
+         },
+             {
+               data:min_temp,
+               borderColor:'#FF3D00',
+               fill:false
+             },
+              ]
+        },
+               options:{
+                 elements: {
+                   line: {
+                       tension: 0
+                       }
+                 },
+                 legend:{
+                   display:false
+                },
+                 scales:{
+                   xAxes:[{
+                     display:true
+                   }],
+                   yAxes:[{
+                     display:true
+                   }] }
+                 } 
+       })
+        }) 
      }
     //  Filter (event:any) {
     //   alert( event);
  
     // }
           ngOnInit(){
-            
-            this._weather.daylyForecast()
-             .subscribe(res=>{
-              let max_temp=res['data'].map(res=>res.max_temp)
-              let min_temp=res['data'].map(res=>res.min_temp)
-              let alldates=res['data'].map(res=>res.datetime)
-              //console.log(alldates)
-         let weatherDates=[]
-              alldates.forEach((res)=>{
-                let jsdate=new Date(res )
-                weatherDates.push(jsdate.toLocaleDateString('en',{year: 'numeric', month: 'long', day: 'numeric' }))
-         })
-
-             
-
-             
-          this.chart=new Chart('canvas',{
-          type:'line',
-          data:{
-            labels:weatherDates,
-              datasets:[{
-                data:max_temp,
-                borderColor:'#0288D1',
-                fill:false
-              },
-                  {
-                    data:min_temp,
-                    borderColor:'#FF3D00',
-                    fill:false
-                  },
-                   ]
-             },
-                    options:{
-                      elements: {
-                        line: {
-                            tension: 0
-                            }
-                      },
-                      legend:{
-                        display:false
-                     },
-                      scales:{
-                        xAxes:[{
-                          display:true
-                        }],
-                        yAxes:[{
-                          display:true
-                        }] }
-                      } 
-            })
-          
-             
-            })
+            this.ciudadcodigo = null;
+    this.unidad= "SELECT";
+    this.jsonList= [  {"ciudadcodigo" : "4013704", "unidad" : "I"},      {"ciudadcodigo" : "4013702",       "unidad" : "M"}  ]
+        
           }
+         
+    
          
         }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import{HttpClient, HttpHeaders} from '@angular/common/http';
 import{Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+ 
+import { Chart } from 'chart.js';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  maxima:any[ ];
+  response:any[ ];
   
+  chart=[ ];
  
   daylyForecast(): any {
     return this._http.get("https://api.weatherbit.io/v2.0/forecast/daily?days=16&city_id=4013704&units=M&key=3dd1694285a347d4ac8c5c20608f68de")
@@ -15,17 +21,68 @@ export class WeatherService {
   }
 
 
+  default() {
   
+    this.daylyForecast()
+        .subscribe((res)=> {
+          let response=res['data'] ;
+          this.maxima=response  
+          this.response=response
+         //console.log(this.response);
+         let max_temp=res['data'].map(res=>res.max_temp)
+         let min_temp=res['data'].map(res=>res.min_temp)
+         let alldates=res['data'].map(res=>res.valid_date)
+         // console.log(alldates)
+         
+     this.chart=new Chart('canvas',{
+     type:'line',
+     data:{
+       labels:alldates,
+         datasets:[{
+           data:max_temp,
+           borderColor:'#0288D1',
+           fill:false
+         },
+             {
+               data:min_temp,
+               borderColor:'#FF3D00',
+               fill:false
+             },
+              ]
+        },
+               options:{
+                 elements: {
+                   line: {
+                       tension: 0
+                       }
+                 },
+                 legend:{
+                   display:false
+                },
+                 scales:{
+                   xAxes:[{
+                     display:true
+                   }],
+                   yAxes:[{
+                     display:true
+                   }] }
+                 } 
+       })
+        }) 
+  
+     
+  }
 getCities() {
-   return ['4013704',
-  '3995019',
-  '4004898',
-  '4004886'
-]
+  
+  return [  {"ciudad" : "Navojoa", "codigo" : "3995019"},{"ciudad" : "Hermosillo", "codigo" : "4004898"},{"ciudad" : "Nogales", "codigo" : "4004886"} ]
 
-
+   
 }
 
+getUnits() {
+  return ['M',
+ 'I' 
+]}
 
 
   constructor(private _http: HttpClient) { }
